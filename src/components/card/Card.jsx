bc19b48ef8eae95deb11fc1a53f2ea3a4aa57f46
Javi Villar <javiroll93@gmail.com>
2023-08-11 16:40:14 +0200

    flip card effect

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -5,23 +5,33 @@ import { CARD_STATES } from "../../constants/constants"
 export default function Card(props) {
     const { number, state, checkNumber, blocked } = props
     const [numberRender, setNumberRender] = useState('')
+    const [cardState, setCardState] = useState()
 
 
     const revealCard = () => {
+        setCardState(CARD_STATES.VISIBLE)
         if (blocked)
             return
-            
+
         setNumberRender(number)
         checkNumber()
     }
 
     useEffect(() => {
         (state === CARD_STATES.HIDDEN) ? setNumberRender('') : setNumberRender(number)
+        setCardState(state)
     }, [state, number])
 
     return (
-        <div className={style.card} onClick={revealCard}>
-            {numberRender}
+        <div className={`${style.card} ${(cardState === CARD_STATES.HIDDEN) ? style.hidden : ''}`}
+            onClick={revealCard}>
+            <div className={style.innerCard}>
+                <div className={style.cardFront}>
+                    {numberRender}
+                </div>
+                <div className={style.cardBack}>
+                </div>
+            </div>
         </div>
     )
 }
