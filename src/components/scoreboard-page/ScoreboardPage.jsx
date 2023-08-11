6031e4d088a3029077f9f15014c241cf51d8d397
Javi Villar <javiroll93@gmail.com>
2023-08-11 18:45:28 +0200

    socreboard styling

--- src/components/scoreboard-page/ScoreboardPage.jsx
+++ src/components/scoreboard-page/ScoreboardPage.jsx
@@ -1,13 +1,27 @@
 import { Link } from "react-router-dom"
+import style from "./scoreboardPage.module.css"
 
 export default function ScoreboardPage() {
-    const scoreData = window.localStorage.getItem('scoreboard') || ''
+    const scoreDataRaw = window.localStorage.getItem('scoreboard') || '[]'
+    const scoreData = JSON.parse(scoreDataRaw)
 
     return (
-        <section>
-            <span>
-                {scoreData}
-            </span>
+        <section className={style.scoreBoard}>
+            <h1>Players</h1>
+            <table>
+                <tbody>
+                    {scoreData.map((data, i) => {
+                        return (
+                            <tr key={i}>
+                                <td>{i+1}</td>
+                                <td className={style.playerName}>{data.player}</td>
+                                <td className={style.date}>{data.date}</td>
+                                <td className={style.score}>{data.score}</td>
+                            </tr>
+                        )
+                    })}
+                </tbody>
+            </table>
             <Link to='/'>Back home</Link>
         </section>
     )
