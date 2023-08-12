6713d87030cab8020ae695eb7b9a1b8afe233a16
Javi Villar <javiroll93@gmail.com>
2023-08-12 12:03:36 +0200

    more navigation tests

--- src/test/navigation.test.jsx
+++ src/test/navigation.test.jsx
@@ -1,53 +1,106 @@
 import { cleanup, render, fireEvent, screen } from "@testing-library/react"
-import { it, expect } from "vitest"
+import { it, expect, describe } from "vitest"
 import { MemoryRouter } from "react-router-dom"
 import App from "../App"
 
-it('should navigate to "Scoreboard page" from "Home page" when click on "scoreboard" link', () => {
-    cleanup()
-    
-    render(
-        <MemoryRouter initialEntries={['/']}>
-            <App />
-        </MemoryRouter>
-    )
+describe("navigation behavior", () => {
+    it('should render the Home Page on "/" route', () => {
+        cleanup()
 
-    const scoreboardLink = screen.getByText('Score board')
-    fireEvent.click(scoreboardLink)
+        render(
+            <MemoryRouter initialEntries={['/']}>
+                <App />
+            </MemoryRouter>
+        )
 
-    expect(screen.getByRole('table'))
-    expect(screen.getByText('Players'))
-})
+        expect(screen.getByText('Memory Cards Game'))
+    })
 
-it('should navigate to "Game Page" from "Home page" when click on "Continue" button after introduce player name', () => {
-    cleanup()
-    
-    render(
-        <MemoryRouter initialEntries={['/']}>
-            <App />
-        </MemoryRouter>
-    )
+    it('should render the Scoreboard Page on "/score-board" route', () => {
+        cleanup()
 
-    const input = screen.getByRole('textbox')
-    fireEvent.change(input, {target: {value: 'Player1'}})
+        render(
+            <MemoryRouter initialEntries={['/score-board']}>
+                <App />
+            </MemoryRouter>
+        )
 
-    const continueButton = screen.getByText('Continue')
-    fireEvent.click(continueButton)
+        expect(screen.getByText('Players'))
+        expect(screen.getByRole('table'))
+    })
 
-    expect(screen.getByRole('combobox')) // <-- Difficulty selector
-})
+    it(`shouldn't render the Game Page on "/game" route accessing directly and should show the Home Page instead`, () => {
+        cleanup()
 
-it(`shouldn't navigate to "Game Page" from "Home page" when click on "Continue" button without introduce player name`, () => {
-    cleanup()
-    
-    render(
-        <MemoryRouter initialEntries={['/']}>
-            <App />
-        </MemoryRouter>
-    )
+        render(
+            <MemoryRouter initialEntries={['/game']}>
+                <App />
+            </MemoryRouter>
+        )
 
-    const continueButton = screen.getByText('Continue')
-    fireEvent.click(continueButton)
+        expect(() => screen.getByRole('combobox')).toThrow() // <-- Difficulty selector
 
-    expect( () => screen.getByRole('combobox')).toThrow() // <-- Difficulty selector
+        expect(screen.getByText('Memory Cards Game'))
+    })
+
+    const randomRoute = `/rand${new Date().getTime()}`
+    it(`should render the Home Page on the random route "${randomRoute}"`, () => {
+        cleanup()
+
+        render(
+            <MemoryRouter initialEntries={[randomRoute]}>
+                <App />
+            </MemoryRouter>
+        )
+    })
+
+    it('should navigate to "Scoreboard page" from "Home page" when click on "scoreboard" link', () => {
+        cleanup()
+
+        render(
+            <MemoryRouter initialEntries={['/']}>
+                <App />
+            </MemoryRouter>
+        )
+
+        const scoreboardLink = screen.getByText('Score board')
+        fireEvent.click(scoreboardLink)
+
+        expect(screen.getByRole('table'))
+        expect(screen.getByText('Players'))
+    })
+
+    it('should navigate to "Game Page" from "Home page" when click on "Continue" button after introduce player name', () => {
+        cleanup()
+
+        render(
+            <MemoryRouter initialEntries={['/']}>
+                <App />
+            </MemoryRouter>
+        )
+
+        const input = screen.getByRole('textbox')
+        fireEvent.change(input, { target: { value: 'Player1' } })
+
+        const continueButton = screen.getByText('Continue')
+        fireEvent.click(continueButton)
+
+        expect(screen.getByRole('combobox')) // <-- Difficulty selector
+    })
+
+    it(`shouldn't navigate to "Game Page" from "Home page" when click on "Continue" button without introduce player name`, () => {
+        cleanup()
+
+        render(
+            <MemoryRouter initialEntries={['/']}>
+                <App />
+            </MemoryRouter>
+        )
+
+        const continueButton = screen.getByText('Continue')
+        fireEvent.click(continueButton)
+
+        expect(() => screen.getByRole('combobox')).toThrow() // <-- Difficulty selector
+    })
+
 })
